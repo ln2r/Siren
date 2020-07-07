4a42b63791d128c384335c443eefbf9fc6e9e245
Joshua V. Lewis <Darkwater409@gmail.com>
2020-07-07 10:56:34 -0700

    Update code

--- commands/help.js
+++ commands/help.js
@@ -1,19 +1,10 @@
-/*
-The HELP command is used to display every command's name and description
-to the user, so that he may see what commands are available. The help
-command is also filtered by level, so if a user does not have access to
-a command, it is not shown to them. If a command name is given with the
-help command, its extended help is shown.
-*/
-
 exports.run = (client, message, args, level) => {
-  // If no specific command is called, show all filtered commands.
+
   if (!args[0]) {
-    // Filter all commands by which are available for the user's level, using the <Collection>.filter() method.
+   
     const myCommands = message.guild ? client.commands.filter(cmd => client.levelCache[cmd.conf.permLevel] <= level) : client.commands.filter(cmd => client.levelCache[cmd.conf.permLevel] <= level &&  cmd.conf.guildOnly !== true);
 
-    // Here we have to get the command names only, and we use that array to get the longest name.
-    // This make the help commands "aligned" in the output.
+
     const commandNames = myCommands.keyArray();
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
 
@@ -30,7 +21,7 @@ exports.run = (client, message, args, level) => {
     });
     message.channel.send(output, {code: "asciidoc", split: { char: "\u200b" }});
   } else {
-    // Show individual command's help.
+
     let command = args[0];
     if (client.commands.has(command)) {
       command = client.commands.get(command);
--- commands/reboot.js
+++ commands/reboot.js
@@ -1,4 +1,4 @@
-exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+exports.run = async (client, message, args, level) => { 
   await message.reply("Bot is shutting down.");
   await Promise.all(client.commands.map(cmd =>
     client.unloadCommand(cmd)
